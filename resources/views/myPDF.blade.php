c5f229b37cc1bc6a18ca1491b63ef9b1a705b8c2
Daniel leal <daniel.felipe.lg@hotmail.com>
2024-02-15 15:19:46 -0500

    changes

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -18,8 +18,8 @@
     {{-- <br> --}}
     <p class="title">  Personería Jurídica, Resolución 598 del 2 de abril de 2001 del Ministerio de Educación Nacional - Registro ICFES 2738</p>
   </p>
-  <div class="title-center">
-    <h3 class="titleCertificado">{{ $title }}</h3>
+  <div style=" width: 100%;  margin: 0 auto;  text-align: center;">
+    <h3 >{{ $title }}</h3>
   </div>
             <p class="textContent "> Que el señor/a <b class="container">{{ $name }}</b> identificado/a con <b> {{ $t_doc }} </b> No. <b>{{ $document }}</b>, está vinculado/a con la Fundación Universitaria Empresarial de la Cámara de Comercio de Bogotá Uniempresarial con NIT 830.084.876-6, desempeñando el cargo de <b>{{ $contract->posts->name }}</b>.
             {{$type_contract}}
