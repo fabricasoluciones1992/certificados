551aa72f7cf71c33573a59a1400f65e7eaa1642e
Daniel leal <daniel.felipe.lg@hotmail.com>
2024-05-06 11:49:09 -0500

    Add show password in edit password profile

--- resources/views/users/edit_password.blade.php
+++ resources/views/users/edit_password.blade.php
@@ -3,6 +3,7 @@
   Registro
 @endsection
 @section('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <div class="d-grid gap-2 d-md-flex justify-content-end m-5">
     <a href="{{route('users.show', Auth::id())}}"><button class="btn btn-danger" id=""><i class="fa-solid fa-arrow-left px-3 justify-content-center"></button></i></button></a>
   </div>
@@ -21,11 +22,17 @@
                         @csrf
                         @method('PUT')
                         <div class="row my-3">
+                            
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña actual') }}</label>
 
                             <div class="col-md-6">  
+                                <div class="input-group">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
-
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button" onclick="mostrarPassword('password')"> <span class="fa fa-eye-slash icon"></span> </button>
+                                </div>                                
+                            </div>
+                            
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -37,8 +44,13 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Nueva contraseña') }}</label>
 
                             <div class="col-md-6">  
-                                <input id="password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" autocomplete="new-password">
-
+                                <div class="input-group">
+                                    <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" autocomplete="new-password">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button" onclick="mostrarPassword('new_password')"> <span class="fa fa-eye-slash icon"></span> </button>
+                                    </div>
+                                </div>
+                                
                                 @error('new_password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -50,7 +62,13 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Confirmar contraseña') }}</label>
 
                             <div class="col-md-6">  
-                                <input id="password" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" autocomplete="new-password">
+                                <div class="input-group">
+                                    <input id="password_confirmation_field" type="password" class="form-control @error('password_confirmation') is-invalid @enderror" name="password_confirmation" autocomplete="new-password">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button" onclick="mostrarPassword('password_confirmation_field')"> <span class="fa fa-eye-slash icon"></span> </button>
+                                    </div>                                    
+                                </div>
+                                
                                 <p class="fs-7 text-secondary">La contraseña debe contener mínimo 8 caracteres y máximo 15 caracteres, dentro de los cuales debe haber por lo menos una letra, un número y un carácter especial</p>
                                 @error('password_confirmation')
                                     <span class="invalid-feedback" role="alert">
@@ -66,6 +84,20 @@
                                 </button>
                             </div>
                         </div>
+                        <script type="text/javascript">
+                            function mostrarPassword(targetId) {
+                                var cambio = document.getElementById(targetId);
+                                if (cambio.type == "password") {
+                                    cambio.type = "text";
+                                    $('#' + targetId + ' .icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+                                } else {
+                                    cambio.type = "password";
+                                    $('#' + targetId + ' .icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+                                }
+                            }
+
+                        </script>
+
                     </form>
                 </div>
             </div>
