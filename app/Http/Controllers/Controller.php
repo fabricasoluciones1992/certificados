cf6851966d2b4a9d629a44a2998a70853e479b2c
Dflg2003 <109538814+Dflg2003@users.noreply.github.com>
2024-06-26 14:19:32 -0500

    Correction in val code in function ValCode in cotroller general

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class Controller extends BaseController
@@ -22,9 +23,18 @@ class Controller extends BaseController
     public function valCode(Request $request) {
         $decoded_data = base64_decode($request->valCode);
         $values = explode(',', $decoded_data);
-        $contract = Contract::find($values[2]);
-        if (!empty($contract)) {
-            $user = User::find($contract->id_users);
+        if (!empty($contract) || !empty($values[2])) {
+            $sql = "SELECT * FROM contracts where id = $values[2]";
+            try {
+                $contract = DB::select($sql);
+            } catch (\Throwable $th) {
+                $error = [
+                    'title' => "El contrato es inválido.",
+                    'message' => "El contrato que usted busca es inválido."
+                ];
+                return redirect()->back()->with('error', $error);
+            }
+            $user = User::find($contract[0]->id_users);
             $error = [
                 'title' => "El contrato es válido.",    
                 'message' => "El contrato fue validado y generado en la fecha: $values[1], para la persona: $user->name, identificada con el número de documento: $user->document."
